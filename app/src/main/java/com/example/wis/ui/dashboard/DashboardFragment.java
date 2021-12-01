3f8ef833544c5fa5c18ba5430fa65032cc46ce18
Vašek <wasekva@gmail.com>
2021-12-01 12:04:06 +0100

    Added bototm menu

---
+++ app/src/main/java/com/example/wis/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,45 @@
+package com.example.wis.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.wis.databinding.FragmentDashboardBinding;
+
+public class DashboardFragment extends Fragment {
+
+    private com.example.wis.ui.dashboard.DashboardViewModel dashboardViewModel;
+    private FragmentDashboardBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                new ViewModelProvider(this).get(com.example.wis.ui.dashboard.DashboardViewModel.class);
+
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textDashboard;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
