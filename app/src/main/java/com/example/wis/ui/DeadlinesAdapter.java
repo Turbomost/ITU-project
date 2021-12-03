a7b86d0f8ba8125ab0fa3ede87f8764ebbef00a6
xbella01 <xbella01@stud.fit.vutbr.cz>
2021-12-03 16:54:08 +0100

    f

--- app/src/main/java/com/example/wis/ui/DeadlinesAdapter.java
+++ app/src/main/java/com/example/wis/ui/DeadlinesAdapter.java
@@ -4,21 +4,26 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.wis.CustomAdapter;
 import com.example.wis.DeadlineModel;
 import com.example.wis.DeadlineViewModel;
 import com.example.wis.R;
 
 import java.util.List;
 
-public class DeadlinesAdapter extends RecyclerView.Adapter<DeadlineViewHolder>{
+public class DeadlinesAdapter extends RecyclerView.Adapter<DeadlinesAdapter.ViewHolder> {
 
     Context context;
     List<DeadlineViewModel> deadlineList;
 
+    TextView colTime;
+    TextView colName;
+    TextView colSubject;
 
     public DeadlinesAdapter(Context context, List<DeadlineViewModel> deadlineList){
         this.context = context;
@@ -30,24 +35,33 @@ public class DeadlinesAdapter extends RecyclerView.Adapter<DeadlineViewHolder>{
     }
     @NonNull
     @Override
-    public DeadlineViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.myrow,parent,false);
-        DeadlineViewHolder viewHolder = new DeadlineViewHolder(view);
+        ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull DeadlineViewHolder viewHolder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         DeadlineViewModel deadline = deadlineList.get(position);
-        viewHolder.colTime.setText(String.valueOf(deadline.getDeadline_time()));
-        viewHolder.colName.setText(String.valueOf(deadline.getDeadline_name()));
-        viewHolder.colSubject.setText(String.valueOf(deadline.getSubject_name()));
+        colTime.setText(String.valueOf(deadline.getDeadline_time()));
+        colName.setText(String.valueOf(deadline.getDeadline_name()));
+        colSubject.setText(String.valueOf(deadline.getSubject_name()));
     }
+
 
     @Override
     public int getItemCount() {
         return deadlineList.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            colTime = itemView.findViewById(R.id.item_time);
+            colName = itemView.findViewById(R.id.item_name);
+            colSubject = itemView.findViewById(R.id.item_subject);
+        }
     }
 
 }
